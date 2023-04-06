d1d8eb29e9ce882df4301334d524ec2dd877e364
Sam(RYZEN-PC) <abdusamadov55@gamil.com>
2023-04-06 17:32:51 +0900

    Add insert full realization 100% coverage

--- src/binary_search_tree.py
+++ src/binary_search_tree.py
@@ -1,17 +1,36 @@
 from src.node import Node
+
+
 class BinarySearchTree:
     def __init__(self) -> None:
         """Initialing empty Binary Search Tree """
         self.__root = None
 
     @property
-    def root(self):
-        """Returnin url to the top bst"""
+    def root(self) -> Node | None:
+        """Returning url to the top bst"""
         return self.__root
 
     def insert(self, data: dict) -> None:
         """Adding data to the structure Binary Search Tree"""
         if self.__root is None:
             self.__root = Node(data)
+        else:
+            self._insert_recursively(self.__root, data)
+
+    def _insert_recursively(self, node: Node, data: dict):
+        """recursively looking for place for putting new data"""
+        if data['id'] < node.data['id']:
+            if node.left is None:
+                node.left = Node(data)
+            else:
+                self._insert_recursively(node.left, data)
+
+        if data['id'] > node.data['id']:
+            if node.right is None:
+                node.right = Node(data)
+            else:
+                self._insert_recursively(node.right, data)
+
     def search(self, post_id: int) -> dict:
         """Searching and returning dictionary by his id"""
--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -1,10 +1,21 @@
+import pytest
+
 from src.binary_search_tree import BinarySearchTree
 
 
-def tes_bst_empty():
+@pytest.fixture
+def bst_root():
+    """Creating Bst with one top"""
+    bst = BinarySearchTree()
+    bst.insert({'id': 40})
+    return bst
+
+
+def test_bst_empty():
     """Empty binary search tree"""
     bst = BinarySearchTree()
     assert bst.root is None
+
 
 def test_bst_root():
     """Adding data to the top of bst"""
@@ -13,3 +24,26 @@ def test_bst_root():
     bst.insert(data)
     assert bst.root.data == data
 
+
+def test_bst_insert_left(bst_root):
+    """Adding data to the left from the top"""
+    bst_root.insert({'id': 30})
+    assert bst_root.root.left.data['id'] == 30
+
+
+def test_bst_insert_right(bst_root):
+    """Adding data to the right from the top"""
+    bst_root.insert({'id': 50})
+    assert bst_root.root.right.data['id'] == 50
+
+def test_bst_insert_left_left(bst_root):
+    """Adding data to the left 2 times from the top"""
+    bst_root.insert({'id': 30})
+    bst_root.insert({'id': 25})
+    assert bst_root.root.left.left.data['id'] == 25
+
+def test_bst_insert_right_right(bst_root):
+    """Adding data to the right 2 times from the top"""
+    bst_root.insert({'id': 50})
+    bst_root.insert({'id': 60})
+    assert bst_root.root.right.right.data['id'] == 60
